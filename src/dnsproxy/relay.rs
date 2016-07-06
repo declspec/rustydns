19c20a5d5ba7f383e0f43ff31319c2559f50f8e6
Jason Larke <jasonlarke@live.com.au>
2016-07-06 17:43:28 +0800

    Added a try_or_continue macro to stop the program crashing on non-fatal errors and resume the loop

--- src/dnsproxy/relay.rs
+++ src/dnsproxy/relay.rs
@@ -2,6 +2,22 @@ use std::net::ToSocketAddrs;
 use std::net::UdpSocket;
 use std::io::Error;
 
+macro_rules! try_or_continue {
+    ($expr:expr, $msg:tt) => (match $expr {
+        Ok(val) => val,
+        Err(err) => {
+            println!("{}: {}", $msg, err);
+            continue;
+        }
+    });
+    ($expr:expr) => (match $expr {
+        Ok(val) => val,
+        Err(err) => {
+            continue;
+        }
+    });
+}
+
 pub trait Interceptor {
     fn intercept(&self, _: &[u8]) -> Option<Vec<u8>> { None }
 }
@@ -29,16 +45,16 @@ impl<A: ToSocketAddrs, I: Interceptor> UdpRelay<A, I> {
         let mut obuffer = vec![0; capacity];
 
         loop {
-            let (len, client) = try!(master.recv_from(&mut ibuffer));
+            let (len, client) = try_or_continue!(master.recv_from(&mut ibuffer));
             let received = &ibuffer[..len]; 
 
             if let Some(vec) = self.interceptor.intercept(received) {
-                try!(master.send_to(&vec, client));
+                try_or_continue!(master.send_to(&vec, client), "error sending to client");
             }
             else {
-                try!(slave.send_to(received, &self.target));
-                let (len, _) = try!(slave.recv_from(&mut obuffer));
-                try!(master.send_to(&obuffer[..len], client));
+                try_or_continue!(slave.send_to(received, &self.target), "error forwarding to server");
+                let (len, _) = try_or_continue!(slave.recv_from(&mut obuffer), "error receiving from server");
+                try_or_continue!(master.send_to(&obuffer[..len], client), "error sending to client");
             }
         }
     }
